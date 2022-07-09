8e6228f54b4d099d77bd999fb6198ac52f3521e5
SinitYT <sinityonas@outlook.com>
2022-07-09 20:43:39 +0000

    Add a function for the computer to make a random choice of one of the choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const buttons = document.getElementsByTagName("button");
+let comTurn = document.getElementById("com-choice");
 
 
 
@@ -8,10 +9,31 @@ document.addEventListener("DOMContentLoaded",function(){
     for (let button of buttons){
         button.addEventListener("click",function(){
           let userChoice = this.getAttribute("draw-type");
-          console.log(userChoice);
+          computerChoice();
+          
         })
     }
 })
+
+function computerChoice() {
+    /*let choices = ['rock','paper','scissors'];*/
+   
+
+    let imgArray = new Array();
+
+    imgArray[0] = new Image();
+    imgArray[0].src = './images/rock.jpg';
+    
+    imgArray[1] = new Image();
+    imgArray[1].src = './images/paper.png';
+    
+    imgArray[2] = new Image();
+    imgArray[2].src = './images/scissors.png';
+    let random = Math.floor(Math.random()*3);
+    comTurn.innerHTML = imgArray[random];
+    return comTurn;
+
+}
 
 
 
