97015ae0bd7618d3afbce6eec07ca346bdcb61ab
SinitYT <sinityonas@outlook.com>
2022-07-15 11:43:10 +0000

    Change the outlook of game by changing the structure of the index.html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded",function(){
 function computerChoice() {
     /*let choices = ['rock','paper','scissors'];*/
    
-
     let imgArray = new Array();
 
     imgArray[0] = new Image();
@@ -35,13 +34,41 @@ function computerChoice() {
     
     imgArray[2] = new Image();
     imgArray[2].src = "assets/images/scissors.png";
-    let random = Math.floor(Math.random()*3);
-    /*let randomChosenPic = imgArray[random];*/
+    let randomNum = Math.floor(Math.random()*3);
+    switch (randomNum){
+        case 0:
+            comTurn.appendChild(imgArray[0]);
+            break;
+        case 1:
+            comTurn.appendChild(imgArray[1]);
+            break;
+        case 2:
+            comTurn.appendChild(imgArray[2]);
+            break;
+
+    }
+    
+    /*let randomChosenPic = imgArray[random];
     comTurn.appendChild(imgArray[random]);
-    return comTurn;
+    return comTurn;*/
 
 }
 
+
+function winnerCheck(){
+    let userChoice = document.getElementsByClassName('draw-type');
+    if ( userChoice.innerHTML === 'rock' && comTurn.innerHTML === imgArray[0]){
+        announce_p.innerHTML = "It is a tie.";
+    } else if (userChoice.innerHTML === 'rock' && comTurn.innerHTML === imgArray[1]) {
+        announce_p.innerHTML = "Com wins";
+    } else if (userChoice.innerHTML === 'rock' && comTurn.innerHTML === imgArray[2]) {
+        announce_p.innerHTML = "You win";
+    } else {
+        announce_p.innerHTML = "You have to shoot!";
+    }
+
+}
+/*
 function winnerCheck(){
     if (this.getAttribute('draw-type') === 'rock' && comTurn.innerHTML === imgArray[0]){
         console.log("Draw");
@@ -52,6 +79,7 @@ function winnerCheck(){
     }
 
 }
+*/
 
 
 
